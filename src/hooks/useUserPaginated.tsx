c0e9563ab7b59588ed855d109d9f6f471456f780
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-09-04 05:15:46 -0500

    update changes

--- src/hooks/useUserPaginated.tsx
+++ src/hooks/useUserPaginated.tsx
@@ -7,11 +7,8 @@ export const useUserPaginated = () => {
 
     const [simpleUserList, setSimpleUserList] = useState<SimpleUsers[]>([])
 
-    const url = "https://jsonplaceholder.typicode.com/users";
-
-
     const loadUsers = async () => {
-        const resp = await userApi.get(url);
+        const resp = await userApi.get(`https://jsonplaceholder.typicode.com/users`);
         mapUserList(resp.data);
     }
 
@@ -23,22 +20,21 @@ export const useUserPaginated = () => {
     const mapUserList = async (userList: UsersResponse[]) => {
 
         const filtered = userList.filter(({ id }) => id <= 5);
-        const usersPostsPromises = filtered.map((u) => {
-            return getPostsOfUser(u.id)
+        const usersPostsPromises = filtered.map((user) => {
+            return getPostsOfUser(user.id)
         })
         let allRealPosts: PostResponse[] = []
         const allPosts = await Promise.all(usersPostsPromises)
-        allPosts.map(p => {
-            allRealPosts.push(...p)
+        allPosts.map(post => {
+            allRealPosts.push(...post)
         })
-        const userPosts = filtered.map((u) => (
+        const userPosts = filtered.map((user) => (
             {
-                ...u,
-                picture: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${u.id}.png`,
-                publications: allRealPosts.filter(publications => publications.userId === u.id).slice(0, 3)
+                ...user,
+                picture: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${user.id}.png`,
+                publications: allRealPosts.filter(publications => publications.userId === user.id).slice(0, 3)
             }
         ))
-        console.log("userPosts ", userPosts)
         setSimpleUserList(userPosts)
     }
 
