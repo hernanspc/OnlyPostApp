44c44f3af1ccc21709ef3dc208d143523dce1767
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-09-05 16:28:40 -0500

    finish test

--- src/components/__tests__/post.test.tsx
+++ src/components/__tests__/post.test.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import { View, Text } from 'react-native'
 import { shallow } from 'enzyme';
 import PostCard from '../post';
-import { TouchableOpacity } from 'react-native-gesture-handler';
 
-const result = 'I was Pressed';
-const mockFn = jest.fn(() => result);
 describe('Testing Component SearchBox', () => {
 
     const dataMock = [
