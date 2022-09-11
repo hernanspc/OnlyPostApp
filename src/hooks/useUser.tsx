cf98c66b91495b23cec569ceb8c7c40a436f1e3c
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-09-11 11:22:42 -0500

    refactor

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -3,26 +3,27 @@ import { userApi } from '../api/userApi';
 import { UsersResponse, SimpleUsers, PostResponse } from '../interfaces/userInterfaces';
 import { getDataAsyncStorage, removeDataAsyncStorage, saveDataAsyncStorage } from '../utils/storage';
 
+export const getUsers = async () => {
+    const resp = await userApi.get('https://jsonplaceholder.typicode.com/users')
+    return await resp.data
+}
+
+export const getPostsOfUser = async (id: string) => {
+    const resp = await userApi.get('https://jsonplaceholder.typicode.com/users/' + id + '/posts')
+    const value = resp.data;
+    const arrayPosts = value.map((e: any) => (
+        { ...e, imagenPublicacion: `https://res.cloudinary.com/dd0myqhyb/image/upload/v1662351177/OnlyPost/post/post${e.id}.jpg` }
+    ));
+    return await arrayPosts;
+}
+
 export const useUserHook = () => {
     const [simpleUser, setSimpleUser] = useState<SimpleUsers[]>([]);
-
-    const getUsers = async () => {
-        const resp = await userApi.get('https://jsonplaceholder.typicode.com/users')
-        return await resp.data
-    }
-
-    const getPostsOfUser = async (id: string) => {
-        const resp = await userApi.get('https://jsonplaceholder.typicode.com/users/' + id + '/posts')
-        const value = resp.data;
-        const arrayPosts = value.map((e: any) => (
-            { ...e, imagenPublicacion: `https://res.cloudinary.com/dd0myqhyb/image/upload/v1662351177/OnlyPost/post/post${e.id}.jpg` }
-        ));
-        return await arrayPosts;
-    }
 
     const getUsersWithPosts = async () => {
         const storage = await getDataAsyncStorage('@data')
         if (storage) {
+            console.log('storage')
             setSimpleUser(storage);
             return;
         }
@@ -48,6 +49,7 @@ export const useUserHook = () => {
                 isLiked: false,
             }
         ))
+        console.log('fresh')
         saveDataAsyncStorage('@data', userPosts)
         setSimpleUser(userPosts);
     }
